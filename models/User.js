fca9a880b05cb46979d6beab58b2514a895ea810
ShuttlerBad <dileepasudara@gmail.com>
2024-08-28 16:02:54 +0530

    encrypt the password into #

--- models/User.js
+++ models/User.js
@@ -9,6 +9,15 @@ const userSchema = new Schema({
     password : {type: String, required:true}
 });
 
+//encrypt the password into # using pre method
+userSchema.pre("save", async function(next){
+   const user = this;
+   if(!user.isModified) return next();
+   let salt = await bcrypt.genSalt(10);
+   let hash = await bcrypt.hash(user.password,salt);
+   user.password = hash;
+   next(); 
+});
 
 const User = mongoose.model("User", userSchema);
 
