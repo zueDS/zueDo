ae25b81fd681b9bf8c6c3310dc467979598007d2
ShuttlerBad <dileepasudara@gmail.com>
2024-09-05 19:45:50 +0530

    home landing page

---
+++ client/src/pages/Landing/landing.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Navbar from '../../components/Navbar'
+import { Link } from 'react-router-dom'
+import landing from '../../assets/landing.jpg';
+import styles from './landing.module.css';
+
+function Landing() {
+  return (
+    <div>
+      <Navbar /> 
+      <div className={styles.landing__wrapper}>
+        <div className={styles.landing__text}>
+          <h1>Schedule Your Daily Task With <span class="primaryText">ZueDo!</span></h1>
+          <div className='btnWrapper'>
+          <Link to="/regiser" className="primaryBtn">Register</Link>
+          <Link to="/login" className="secondaryBtn">Login</Link>
+        </div>
+      </div>
+
+      <div className={styles.landing__img}>
+        <img src= {landing} alt="landing"></img>
+      </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Landing
