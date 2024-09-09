0cb190e0f28de437f710dd701122e461404b2262
ShuttlerBad <dileepasudara@gmail.com>
2024-09-09 18:21:17 +0530

    Register page

---
+++ client/src/pages/Auth/Register.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from 'react'
+import styles from './Login.module.css';
+import login from '../../assets/login.png';
+import {Button, Input, message} from 'antd';
+import { Link, useNavigate } from 'react-router-dom';
+import AuthServices from '../../services/authServices';
+import { getErrorMessage }from '../../util/GetError';
+
+function Register() {
+
+const [username, setUsername] = useState("");
+const [password, setPassword] = useState("");
+const [firstName, setFirstName] = useState("");
+const [lastName, setLastName] = useState("");
+const [loading, setLoading] = useState("false");
+const navigate = useNavigate(); 
+
+const handleSubmit = async ()=>{
+  try{
+    setLoading(true);
+    const data = {
+      firstName,
+      lastName,
+      username,
+      password
+    }
+
+    const response = await AuthServices.registerUser(data);
+    console.log(response.data);
+    setLoading(false);
+    message.success("You're Registered Successfully!");
+    navigate('/login');
+
+    }catch(err){
+      console.log(err)
+      message.err(getErrorMessage(err));
+      setLoading(false);
+    }
+  }
+
+return(
+  <div>
+        <div className={styles.login__card}>
+          <img src={login} alt=".."/>
+          <h2>Register</h2>
+          <div className={styles.input__inline__wrapper}>
+            <Input 
+            placeholder="First Name"
+            value={firstName}
+            onChange={(e)=> setFirstName(e.target.value)}/>
+
+            <Input 
+            placeholder="Last Name"
+            style={{marginLeft:'10px'}}
+            value={lastName}
+            onChange={(e)=> setLastName(e.target.value)}/>
+
+          </div>
+        <div className={styles.input__wrapper}>
+          <Input 
+            placeholder="Username"
+            style={{marginBottom:'1rem'}}
+            value={username}
+            onChange={(e)=> setUsername(e.target.value)}/>
+
+            <Input.Password 
+            placeholder="Password"
+            value={password}
+            onChange={(e)=> setPassword(e.target.value)}/>
+        </div>
+
+          <div className={styles.input__info}>
+            Existing User? <Link to="/login">Login</Link>
+          </div>
+          <Button loading={loading} type="primary" size="large" disabled= {!username || !password} onClick={handleSubmit}>Register</Button>
+      </div>
+    </div>
+    )
+  }
+export default Register
