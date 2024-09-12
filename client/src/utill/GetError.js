31c4af6d9108911ba1d30c16fa3c36f09d6ae737
ShuttlerBad <dileepasudara@gmail.com>
2024-09-12 10:03:53 +0530

    deleted

--- client/src/utill/GetError.js
+++
@@ -1,7 +0,0 @@
-export function getErrorMessage (error) {
-    const msg = 
-    (error.response && error.response.data && error.response.data.message) || 
-    error.message ||
-    error.toString ();
-    return msg;
-}
